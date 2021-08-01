9fac2bb80aaae7c2e603ecb44bf96714ed31a8a7
Erne <edainesi@fi.uba.ar>
2021-08-01 11:58:11 -0300

    actualizo desafio

--- desafio4/build/01-DainesiErnesto.js
+++ desafio4/build/01-DainesiErnesto.js
@@ -4,8 +4,7 @@ const rxjs_1 = require("rxjs");
 const input = document.querySelector("#inp");
 const label = document.querySelector("#texto-espejado");
 const observable = rxjs_1.fromEvent(input, "keyup");
-observable.subscribe((e) => {
-    console.log(e);
+observable.subscribe(e => {
     const target = e.target;
     let text = target.value;
     label.innerText = text.split('').reverse().join('');
--- desafio4/src/01-DainesiErnesto.ts
+++ desafio4/src/01-DainesiErnesto.ts
@@ -4,8 +4,7 @@ const input = document.querySelector("#inp");
 const label = document.querySelector("#texto-espejado");
     
 const observable = fromEvent(input as HTMLInputElement, "keyup");
-observable.subscribe((e: Event) => {
-	console.log(e);
+observable.subscribe(e => {
     const target = e.target as HTMLInputElement;
     let text = target.value;
     (label as HTMLLabelElement).innerText = text.split('').reverse().join('');
