e557103ad8fa2240e2749cf489b760463ab0ce89
Erne <edainesi@fi.uba.ar>
2021-08-12 20:18:27 -0300

    termino desafio 7

--- desafio6/app.js
+++ desafio6/app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-class Archivo {
+export class Archivo {
 	constructor(nombreArchivo) {
 		this.nombre = nombreArchivo;
 	}
--- desafio7/app.js
+++ desafio7/app.js
@@ -1,7 +1,21 @@
-const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const fs = require('fs');
+
+const totalVisitas = {items: 0, item: 0};
+
+const leerArchivo = () => {
+	try {
+		const archivo = fs.readFileSync('./productos.txt', "utf-8");
+		return JSON.parse(archivo);
+	} catch (err) {
+		throw new Error(err);
+	}
+}
+const getRandomIndex = (min, max) => {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
 
 const server = app.listen(PORT, () => {
 	console.log(`Servidor inicializado en puerto ${server.address().port}`);
@@ -9,20 +23,25 @@ const server = app.listen(PORT, () => {
 
 app.get('/items', (req, res) => {
 	console.log("Items request");
-	try {
-		const file = fs.readSync("./productos.json", "utf-8");
-		const data = JSON.parse(file.toString())
-		res.send(data);
-	} catch (err) {
-		throw new Error(err);
+	const data = leerArchivo();
+	const productos = [];
+	let cantidad = 0;
+	for (let item of data) {
+		productos.push(item)
+		cantidad++;
 	}
-	res.json(productos.items);
+	res.json({items: productos, cantidad: cantidad});
+	totalVisitas["items"]++;
 })
 
 app.get('/item-random', (req, res) => {
 	console.log("Random item request");
+	const data = leerArchivo();
+	res.json(data[getRandomIndex(0, data.length - 1)]);
+	totalVisitas["item"]++;
 })
 
 app.get('/visitas', (req, res) => {
 	console.log("Visitas request");
+	res.json(totalVisitas);
 })
