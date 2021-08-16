93eb4743bdb8ed405de9b962b61349ac6171ab55
Erne <edainesi@fi.uba.ar>
2021-08-16 17:41:30 -0300

    termino desafio 8

---
+++ desafio8/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+	console.log("Servidor inicializado en puerto 8080");
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const productos = [];
+
+app.get('/', (req, res) => {
+	res.send('<h1>Desafio 8</h1>');
+})
+
+app.get('/api/productos/listar', (req, res) => {
+	if (productos.length > 0) {
+		res.json(productos);
+	} else {
+		res.json({error: 'no hay productos cargados'});
+	}
+})
+
+app.get('/api/productos/listar/:id', (req, res) => {
+	const idProducto = req.params.id;
+	if (!productos[idProducto - 1]) {
+		res.json({error: 'producto no encontrado'})
+	} else {
+		res.json(productos[idProducto - 1]);
+	}
+})
+
+app.post('/api/productos/guardar', (req, res) => {
+	console.log("POST request");
+	const producto = {...req.body, id: productos.length + 1};
+	productos.push(producto);
+	res.json(producto);
+})
+
