9a5e7499fe5cb8836bf5bde5f72a6603f0ab12e8
Erne <edainesi@fi.uba.ar>
2021-11-21 16:41:36 -0300

    agrego funcionalidad para enviar mail en login/logout

--- desafio35/src/controllers/login.controller.ts
+++ desafio35/src/controllers/login.controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import logger from "../logger/winston";
+import {transporter} from "../mailer";
 
 export const failedLogin = (req: Request, res: Response) => {
 	logger.error("Failed Login");
@@ -8,14 +9,42 @@ export const failedLogin = (req: Request, res: Response) => {
 
 export const welcome = (req: Request, res: Response) => {
 	const {user} = req.query;
-	res.render('pages/welcome', {user});
+	const mailOptions = {
+		from: 'Nodejs server',
+		to: 'weston.braun16@ethereal.email',
+		subject: 'Logout',
+		htmt: `<h1>Logged in as ${user}</h1>`
+	};
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			logger.error(err);
+			res.status(500).send('<h1>Failed logout</h1>');
+		} else {
+			logger.info(info);
+			res.render('pages/welcome', {user});
+		}
+	})
 };
 
 export const logout = (req: Request, res: Response) => {
 	const expiredSession: boolean = req.query.sessionExpired === "true";
+	const mailOptions = {
+		from: 'Nodejs server',
+		to: 'weston.braun16@ethereal.email',
+		subject: 'Logout',
+		html: '<h1>Logged out from server</h1>'
+	};
 	req.session.destroy(err => {
 		if (!err && !expiredSession) {
-			res.clearCookie('userSession').render('pages/logout', {sessionExpiredMessage: false});
+			transporter.sendMail(mailOptions, (err, info) => {
+				if (err) {
+					logger.error(err);
+					res.status(500).send('<h1>Failed logout</h1>');
+				} else {
+					logger.info(info);
+					res.clearCookie('userSession').render('pages/logout', {sessionExpiredMessage: false});
+				}
+			})
 		} else if (!err && expiredSession) {
 			const sessionExpiredMessage = "Session expired";
 			res.clearCookie('userSession').render('pages/logout', {sessionExpiredMessage});
